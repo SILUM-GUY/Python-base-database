c846a7c3d37279f912c747bc6bc143ba5a8b8e28
SILUM-GUY <94398309+SILUM-GUY@users.noreply.github.com>
2022-03-18 20:48:04 +0100

    Add files via upload
    To start this database you need to have python and start it with python IDLE

---
+++ Python-base-database.py
@@ -0,0 +1,16 @@
+print ("Benvenuto nel Database V99-4")
+input ()
+print ("pistole")
+print ("fucili")
+print ("prezzo_9mm")
+print ("prezzo_combat")
+print ("prezzo_sns")
+print ("prezzo_anti_pr")
+pistole = "400 pistole disponibili"
+fucili = "Non ancora disponibili"
+anti_pr = "250 disponibili"
+prezzo_9mm = "25000 $"
+prezzo_combat = "20000 $"
+prezzo_sns = "15000$ "
+prezzo_anti_pr = "1000 $"
+       
